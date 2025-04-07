e77d47e405d4d0a0f774baead6f0494ba413affe
klovescoding <47025707+klovescoding@users.noreply.github.com>
2025-04-06 20:53:04 -0400

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ let thisYear = today.getFullYear();
 let small = document.querySelector("small");
 let copyright = document.createElement("p")
 
-copyright.innerHTML = `Copyright &copy; ${thisYear} - My Webpage`
+copyright.innerHTML = `Copyright &copy; ${thisYear} - My Writing Sample`
 small.appendChild(copyright);
 small.style.textAlign = "center";
 
